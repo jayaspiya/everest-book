4392f86b54612eaab858316a2c6b676a65e9ccf7
zayazzp <pzayazz@gmail.com>
2021-08-15 16:02:48 +0545

    :sparkles: Signup Validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,20 +1,20 @@
 ;<template>
   <auth-container>
     <h2>Signup</h2>
-    <form >
+    <form @submit.prevent="registerUser">
       <div class="form-control">
         <label for="email">Email:</label>
-        <input type="email" name="email" placeholder="abc@mail.com" autocomplete="off">
+        <input type="email" name="email" placeholder="abc@mail.com" autocomplete="off" v-model="email" required>
       </div>
       <div class="form-control">
         <label for="password">Password:</label>
-        <input type="password" name="password" placeholder="Password"   >
+        <input type="password" name="password" placeholder="Password" v-model="password" required>
       </div>
       <div class="form-control">
         <label for="confirmPassword">Confirm Password:</label>
-        <input type="password" name="confirmPassword" placeholder="Confirm Password">
+        <input type="password" name="confirmPassword" placeholder="Confirm Password" v-model="confirmPassword" required>
       </div>
-      <button>
+      <button >
         Submit
       </button>
     </form>
@@ -28,6 +28,24 @@ import AuthContainer from "../components/UI/AuthContainer.vue"
 export default {
   components:{
     AuthContainer
+  },
+  data(){
+    return {
+      email: "",
+      password: "",
+      confirmPassword: ""
+    }
+  }, 
+  methods:{
+    registerUser(){
+      if(this.password === this.confirmPassword){
+        console.log("User register")
+        this.$router.push("home")
+      }
+      else{
+        alert("The password and confirm password doesn't match")
+      }
+    }
   }
 }
 </script>
