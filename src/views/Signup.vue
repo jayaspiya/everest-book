25151245c38311b888cca90eab705791442b9a4c
zayazzp <pzayazz@gmail.com>
2021-08-17 16:08:56 +0545

    :sparkles: New Registration form

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,6 +3,22 @@
     <h2>Signup</h2>
     <form @submit.prevent="registerUser">
       <div class="form-control">
+        <label for="fname">First Name</label>
+        <input type="text" name="fname" v-model="firstname" required>
+      </div>
+      <div class="form-control">
+        <label for="lname">Last Name</label>
+        <input type="text" name="lname" v-model="lastname" required>
+      </div>
+      <div class="form-control">
+        <label for="address">Address</label>
+        <input type="text" name="address" v-model="address" required>
+      </div>
+      <div class="form-control">
+        <label for="phone">Contact Number</label>
+        <input type="number" name="phone" v-model="phone" required>
+      </div>
+      <div class="form-control">
         <label for="email">Email:</label>
         <input type="email" name="email" placeholder="abc@mail.com" autocomplete="off" v-model="email" required>
       </div>
@@ -25,6 +41,7 @@
 
 <script>
 import api from "../utils/api.js"
+import Toast from "../utils/Toast.js"
 import AuthContainer from "../components/UI/AuthContainer.vue"
 export default {
   components:{
@@ -33,6 +50,10 @@ export default {
   data(){
     return {
       email: "",
+      firstname: "",
+      lastname: "",
+      address: "",
+      phone: "",
       password: "",
       confirmPassword: ""
     }
@@ -43,20 +64,29 @@ export default {
         try {
           const res = await api.post("user/register/", {
             email: this.email,
-            password: this.password
+            firstname: this.firstname,
+            lastname: this.lastname,
+            address: this.address,
+            phone: this.phone,
+            password: this.password,
+            confirmPassword: this.confirmPassword,
           })
           if(res.status === 201){
             this.$router.push("home")
+            const toast = new Toast("New User registered")
+            toast.show()
           }
           else{
-            alert("something went wrong")
+            const toast = new Toast("Something went wrong", "", "danger")
+            toast.show()
           }
         } catch (error) {
           alert(error)
         }
       }
       else{
-        alert("The password and confirm password doesn't match")
+        const toast = new Toast("Password and Confirm passsword doesnot match", "", "danger")
+        toast.show()
       }
     }
   }
