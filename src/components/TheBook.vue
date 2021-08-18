2a1b5f67220fd8f8943acb108b9e549cfeac4bbb
zayazzp <pzayazz@gmail.com>
2021-08-18 07:19:08 +0545

    :lipstick: Addd Book Grid

--- src/components/TheBook.vue
+++ src/components/TheBook.vue
@@ -1,17 +1,14 @@
 <template>
 <div class="cover">
-
     <img :src="book.imageUrl" :alt="book.title" loading="lazy" v-if="book.imageUrl">
     <img src="../assets/no-front.png" :alt="book.title" loading="lazy" v-else>
 
     <div>
-        
-    <h3>
-        {{book.title}}
-    </h3>
-    <p>{{book.author}}</p>
-    <p class="price">{{book.price}}</p>
-    <p>{{book.synopsis}}</p>
+        <h3 class="title">
+            {{book.title}}
+        </h3>
+        <p class="author">{{book.author}}</p>
+        <p class="price">{{book.price}}</p>
     </div>
 </div>
 </template>
@@ -28,13 +25,17 @@ export default {
 .cover{
     padding: 10px;
     margin: 10px;
-    background: #FFF;;
+    background: var(--alt-color);
+    width: fit-content;
 }
 img{
-    max-height:200px;
+    max-height:250px;
     margin: 0 10px;
 }
 .price::before{
     content: "Rs.";
+}
+.author , .title{
+    color: var(--base-color);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 ;<template>
   <h2>Home page</h2>
-  <TheBook v-for="book in books" :key="book._id" :book="book"/>
+  <div class="book-grid">
+    <TheBook v-for="book in books" :key="book._id" :book="book"/>
+  </div>
 </template>
 <script>
 import api from "../utils/api.js"
@@ -20,3 +22,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.book-grid{
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+}
+</style>
